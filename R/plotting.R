61784b9e7c4d63ab243e64c76cffbea631b8dba1
nibscles <francesco.lescai@nibsc.org>
2020-04-21 15:15:43 +0100

    function and dependencies

--- R/plotting.R
+++ R/plotting.R
@@ -1,4 +1,19 @@
 
+#' Plotting Histogram of Variant consequences
+#'
+#' @param vcf an object of class VcfFile
+#' @param ... additional parameters for the plotting
+#'
+#' @return A \code{\link{ggplot2}} plot object
+#'
+#' @examples
+#' \dontrun{
+#' vcf_histogram(vcf)
+#' }
+#' @import dplyr
+#' @import ggplot2
+#' @importFrom tibble as_tibble
+#'
 vcf_histogram <- function(vcf, ...) {
   require(VariantAnnotation)
   require(dplyr)
